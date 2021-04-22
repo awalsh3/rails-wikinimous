3175567d756d10ec1cba7d893db6a33a9df7d5a9
Annie Walsh <annie@anyonething.co.uk>
2021-04-22 19:33:08 +0100

    files setup and gem fake and simpleform added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning database..."
+Article.destroy_all
+
+puts "Creating articles..."
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words
+    )
+  article.save
+  end
+
+puts "Finished!"
